78f25ba78efc7dc3608448af52d2f149461ecdca
thejayadad <74875595+thejayadad@users.noreply.github.com>
2023-07-19 15:55:03 -0400

    update

--- myapp/app/about/page.js
+++ myapp/app/about/page.js
@@ -3,11 +3,32 @@ import React from 'react'
 const About = () => {
   return (
     <section>
-        <div className='grid grid-rows-2 text-gray-600'>
-                
+        <h2 className='text-center'>About Us</h2>
+        <div className='grid text-gray-600'>
+        <div class="p-4">
+          <img className="rounded mb-6" src="https://images.pexels.com/photos/1785852/pexels-photo-1785852.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2" alt="about" />
+         <div className='grid grid-cols-2'>
+            <div>
+            <h1 class="title-font sm:text-4xl text-3xl mb-4 font-medium text-gray-900">Before they sold out
+            <br class="hidden lg:inline-block" />
+            </h1>
+                <h2>
+                Edison bulb retro cloud bread echo park, helvetica stumptown taiyaki taxidermy 90's cronut +1 kinfolk. Single-origin coffee ennui shaman taiyaki vape DIY tote bag drinking vinegar cronut adaptogen squid fanny pack vaporware. Man bun next level coloring book skateboard four loko knausgaard. Kitsch keffiyeh master cleanse direct trade indigo juice before they sold out gentrify plaid gastropub normcore XOXO 90's pickled cindigo jean shorts. Slow-carb next level shoindigoitch ethical authentic, yr scenester sriracha forage franzen organic drinking vinegar.
+                </h2> 
+            </div>
+            <div class="lg:flex-grow lg:pl-24 md:pl-16 flex flex-col md:items-start md:text-left items-center text-center">
+      <h1 class="title-font sm:text-4xl text-3xl mb-4 font-medium text-gray-900">Before they sold out
+        <br class="hidden lg:inline-block" />
+      </h1>
+      <p class="mb-8 leading-relaxed">Copper mug try-hard pitchfork pour-over freegan heirloom neutra air plant cold-pressed tacos poke beard tote bag. Heirloom echo park mlkshk tote bag selvage hot chicken authentic tumeric truffaut hexagon try-hard chambray.</p>
+      <div class="flex justify-center">
+        <button class="inline-flex text-white bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded text-lg">Button</button>
+        <button class="ml-4 inline-flex text-gray-700 bg-gray-100 border-0 py-2 px-6 focus:outline-none hover:bg-gray-200 rounded text-lg">Button</button>
+      </div>
+    </div>
+         </div>
         </div>
-
-
+        </div>
     </section>
   )
 }
