da60f939af7e88d7032bf0628ff1390951f5f7fc
thejayadad <74875595+thejayadad@users.noreply.github.com>
2023-07-19 14:21:05 -0400

    update

---
+++ myapp/app/featured/page.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Featured = () => {
+  return (
+    <section>
+        <h2>Featured</h2>
+        <div className='flex align-center gap-50 mb-50 bg-gray-300'>
+        test
+        </div>
+    </section>
+  )
+}
+
+export default Featured
